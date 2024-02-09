f9d8d7e727bd3532c4c8e76bb4c7023db358ce38
Manu-HR88 <112296930+Manu-HR88@users.noreply.github.com>
2024-02-09 10:59:10 -0600

    Add files via upload

---
+++ OCI_2024.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: orange;'>SISTEMA DE EVALUACIÓN OCI 2024</h1>", unsafe_allow_html=True)
+
+"*Por*"
+
+"COORDINACIÓN SECTORIAL DE EDUCACIÓN PRIMARIA"
+"SUBDIRECCIÓN DE APOYO TÉCNICO COMPLEMENTARIO"
+
+
+st.markdown("<h3 style='text-align: right; color: orange;'>Ciclo Escolar 2023-2024</h3>", unsafe_allow_html=True)
