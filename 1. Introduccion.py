18dd2538ede51e1a33a8d739b85e7e7d5bdb03c5
Manu-HR88 <112296930+Manu-HR88@users.noreply.github.com>
2024-02-09 11:14:40 -0600

    Add files via upload

---
+++ 1. Introduccion.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: orange;'>¡B i e n v e n i d o!</h1>", unsafe_allow_html=True)
+
+# st.header("¡B i e n v e n i d o!")
+
+st.subheader("Consideraciones")
+
+"""
+Este tablero tiene como finalidad procesar y analizar la información resultante del Concurso Olimpiada del Conocimiento.
+"""
+
+st.subheader("Consideraciones")
+
+"""
+Aquí podemos incluir elementos básicos a considerar para que funcione de la mejor manera el tablero.
+
+Incluso los datos de asistencia técnica en caso de algún problema jajajaja.
+"""
