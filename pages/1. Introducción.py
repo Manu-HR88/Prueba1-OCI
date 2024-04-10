e2feb9ea0f73b72e1af8304c768657294dccdd5c
Manu-HR88 <112296930+Manu-HR88@users.noreply.github.com>
2024-04-10 02:24:04 -0600

    Add files via upload

---
+++ pages/1. Introducción.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+st.image('Logo_SATC_fondo.png', use_column_width=True)
+
+st.markdown("<h1 style='text-align: center; color: #800226; '>¡P R E S E N T A C I Ó N!</h1>", unsafe_allow_html=True)
+
+# st.header("¡B i e n v e n i d o!")
+
+st.subheader("Bienvenido")
+
+st.markdown("<p style='text-align: justify;'>El Sistema Calificador y de Evaluación (SISE) es un sistema que tiene la funcionalidad de calificar las respuestas de las y los estudiantes participantes; mostrar los resultados por alumno, grupo, escuela; identificar los posibles casos de empate y categorizar los Contenidos Curriculares y Proceso de Desarrollo (PDA), del Plan de estudio 2022, evaluados de acuerdo a una prioridad baja, media y alta a través de un mapeo de aprendizajes. Lo anterior con la finalidad de generar un Programa de Reforzamiento del Aprendizaje a corto, mediano y largo plazo que contribuya a una educación de excelencia, priorizando el máximo logro de aprendizajes de niñas, niños y adolescentes (NNA).",unsafe_allow_html=True)
+
+st.markdown("<p style='text-align: justify;'>Para conocer todas sus funcionalidades y beneficios te invitamos a consultar en el menú del lado izquierdo la opción “Guía de usuario”",unsafe_allow_html=True)
+
+st.markdown("<p style='text-align: justify;'><strong><em>¡Esperamos que esta herramienta te sea de gran utilidad!.</em></strong></p>", unsafe_allow_html=True)
+
---
+++ pages/2. Guía de Usuario.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+
+st.image('Logo_SATC_fondo.png', use_column_width=True)
+
+st.markdown("<h1 style='text-align: center; color: #800226; '>GUÍA DE USUARIO</h1>", unsafe_allow_html=True)
+
+
+"""
+*Captura de respuestas por estudiante, por grupo y por escuela.*
+
+Ponemos a tu disposición un archivo electrónico en excel, denominado "OCI2024_Escuela.xlsm", en el cual capturarás las respuestas correctas de tu instrumento, los datos de la escuela, del grupo y del estudiante de sexto grado participante; así como cada una de las respuestas dadas por cada uno de ellos. 
+
+*Para calificar las respuestas*
+
+En la pestaña “Calificador”, ubicada en el menú de la izquierda, deberás adjuntar el archivo electrónico en excel, con la captura de datos y de las respuestas de cada alumno por grupo y escuela. 
+
+Una vez que se haya adjuntado, SISE te mostrará la siguiente información: 
+
+ 1)	El nombre del participante con mayor número de aciertos; así como los datos de identificación con los cuales iniciaste la captura. 
+ 2)	Un botón denominado “Ver resultados de escuelas públicas o particulares”. Al pusarlo, se despliega una tabla con los datos de la escuela, del grupo, del docente, del estudiante, el número de aciertos totales en la prueba y el desglose de aciertos por Campo formativo. Estos resultados se organizan de mayor a menor número de aciertos. Si hubiese empate en esta pantalla se mostrarán dichos resultados.
+
+Es importante señalar que, los resultados no se guardan en el sistema por razones de confidencialidad. Por ello, los resultados se despliegan en pantalla y permanecen visibles siempre y cuando no se actualice la página. 
+
+Considerando lo anterior, se pone a tu disposición el botón *“Descargar archivo de escuelas públicas o privadas”*. Al pulsarlo, se muestra la liga con el nombre *“Haz clic para descargar los resultados en Excel”*. Una vez realizada esta acción, el archivo solicitado se aloja en la carpeta *“Descargas”* de tu equipo de cómputo. 
+
+"""
