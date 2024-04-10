f8588b6cc9bc331afbac2bf41175d0d494d9227b
Manu-HR88 <112296930+Manu-HR88@users.noreply.github.com>
2024-04-10 02:29:00 -0600

    Update OCI_2024.py

--- OCI_2024.py
+++ OCI_2024.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center; color: orange;'>SISTEMA DE EVALUACIÓN OCI 2024</h1>", unsafe_allow_html=True)
+#st.image('https://github.com/Manu-HR88/Prueba1-OCI/blob/main/Logos_SATC.jpg?raw=true', caption='Imagen local', use_column_width=True)
 
-"*Por*"
+st.image('Logo_SATC_fondo.png', use_column_width=True)
+st.markdown("<h1 style='text-align: center; color: #800226; font-size: 36px;'>OLIMPIADA DEL CONOCIMIENTO INFANTIL 2024</h1>", unsafe_allow_html=True)
 
-"COORDINACIÓN SECTORIAL DE EDUCACIÓN PRIMARIA"
-"SUBDIRECCIÓN DE APOYO TÉCNICO COMPLEMENTARIO"
+st.markdown("<h1 style='text-align: center; color: #800226; font-size: 30px;'>Sistema Calificador y de Evaluación</h1>", unsafe_allow_html=True)
 
-
-st.markdown("<h3 style='text-align: right; color: orange;'>Ciclo Escolar 2023-2024</h3>", unsafe_allow_html=True)
