aec3f1cbbcf4a4276283446e63e77ab34430d8fb
Manu-HR88 <112296930+Manu-HR88@users.noreply.github.com>
2024-04-24 17:09:47 -0600

    Update OCI_2024.py

--- OCI_2024.py
+++ OCI_2024.py
@@ -2,7 +2,27 @@ import streamlit as st
 
 #st.image('https://github.com/Manu-HR88/Prueba1-OCI/blob/main/Logos_SATC.jpg?raw=true', caption='Imagen local', use_column_width=True)
 
+# Agrega un texto semi-transparente como marca de agua
+st.markdown(
+    """
+    <style>
+        .watermark {
+            position: fixed;
+            top: 37%;
+            left: 40%;
+            transform: translate(-50%, -50%);
+            color: #C9C9C9;
+            font-size: 80px;
+            transform: rotate(-45deg);
+        }
+    </style>
+    <div class="watermark">Versión de Prueba</div>
+    """,
+    unsafe_allow_html=True
+)
+
 st.image('Logo_SATC_fondo.png', use_column_width=True)
+
 st.markdown("<h1 style='text-align: center; color: #800226; font-size: 36px;'>OLIMPIADA DEL CONOCIMIENTO INFANTIL CICLO ESCOLAR 2023-2024</h1>", unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; color: #800226; font-size: 30px;'>Sistema Calificador y de Evaluación</h1>", unsafe_allow_html=True)
