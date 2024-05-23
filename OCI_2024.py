179c1b855df8f06df715c854141cbcbf21654d63
Manu-HR88 <112296930+Manu-HR88@users.noreply.github.com>
2024-05-22 23:31:06 -0600

    Update OCI_2024.py

--- OCI_2024.py
+++ OCI_2024.py
@@ -3,23 +3,23 @@ import streamlit as st
 #st.image('https://github.com/Manu-HR88/Prueba1-OCI/blob/main/Logos_SATC.jpg?raw=true', caption='Imagen local', use_column_width=True)
 
 # Agrega un texto semi-transparente como marca de agua
-st.markdown(
-    """
-    <style>
-        .watermark {
-            position: fixed;
-            top: 50%;
-            left: 40%;
-            transform: translate(-50%, -50%);
-            color: #C9C9C9;
-            font-size: 80px;
-            transform: rotate(-45deg);
-        }
-    </style>
-    <div class="watermark">Versión de Prueba</div>
-    """,
-    unsafe_allow_html=True
-)
+#st.markdown(
+#    """
+#    <style>
+#        .watermark {
+#            position: fixed;
+#            top: 37%;
+#            left: 40%;
+#            transform: translate(-50%, -50%);
+#            color: #E4E4E4;
+#            font-size: 80px;
+#            transform: rotate(-45deg);
+#        }
+#    </style>
+#    <div class="watermark">Versión de Prueba</div>
+#    """,
+#    unsafe_allow_html=True
+#)
 
 st.image('Logo_SATC_fondo.png', use_column_width=True)
 
